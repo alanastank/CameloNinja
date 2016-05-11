48f49c71cd6d7c1ea6dc7af80f582dca7b301897
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-05-11 17:33:49 -0300

    feat(CameloNinja): adicionar pasta do projeto

---
+++ CameloNinja/CameloNinja/Controllers/PedidoController.cs
@@ -0,0 +1,23 @@
+﻿using CameloNinja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CameloNinja.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET: Pedido
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Resultado(PedidoModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ CameloNinja/CameloNinja/Models/PedidoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CameloNinja.Models
+{
+    public class PedidoModel
+    {
+        public DateTime DataDesejoEntrega { get; set; }
+        public string NomeProduto { get; set; }
+        public int ValorDeVenda { get; set; }
+        public string TipoDePagamento { get; set; }
+        public string NomeCliente { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
