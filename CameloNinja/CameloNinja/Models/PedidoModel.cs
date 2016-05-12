b472aa83efa0cf1c7211815d65ac6409d2e2e397
Marcelo Moura <Marcelo Moura>
2016-05-11 22:38:12 -0300

    feat(PedidoModel): ajustado model com retricoes e nomes corretos

--- CameloNinja/CameloNinja/Models/PedidoModel.cs
+++ CameloNinja/CameloNinja/Models/PedidoModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +9,30 @@ namespace CameloNinja.Models
 {
     public class PedidoModel
     {
+        [Required(ErrorMessage = "Data de Entrega deve ser informada")]
+        [DisplayName("Data desejada de entrega")]
         public DateTime DataDesejoEntrega { get; set; }
+
+        [Required(ErrorMessage = "Nome do Produto deve ser informado")]
+        [DisplayName("Nome do Produto")]
         public string NomeProduto { get; set; }
-        public int ValorDeVenda { get; set; }
+
+        [Required(ErrorMessage = "Valor de venda deve ser informado")]
+        [DisplayName("Valor de Venda")]
+        public float ValorDeVenda { get; set; }
+
+        [Required]
+        [DisplayName("Tipo de Pagamento")]
         public string TipoDePagamento { get; set; }
+
+        [Required(ErrorMessage = "Nome do Cliente deve ser informado")]
+        [DisplayName("Nome do Cliente")]
         public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "Cidade deve ser informada")]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Estado deve ser informado")]
         public string Estado { get; set; }
     }
 }
