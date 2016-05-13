df988d5d0231e6974b59a8aba4696073aeb7fff4
Marcelo Moura <marcelowarth@gmail.com>
2016-05-13 08:04:57 -0300

    Revert "feat(dia05): exercicios"
    This reverts commit dbaa62d364cdb8f160b16786e961f4e4a384f645.

---
+++ CameloNinja/CameloNinja/Controllers/PedidoController.cs
@@ -0,0 +1,29 @@
+﻿using CameloNinja.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CameloNinja.MVC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detalhes(PedidoModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Detalhes", model);
+            }
+            else
+            {
+                return View("Index", model);
+            }
+        }
+    }
+}
